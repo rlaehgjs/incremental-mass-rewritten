babc15112187df1c328db6c77187c192230899af
qq1010903229 <1010903229@qq.com>
2022-08-01 19:15:14 +0800

    commit

--- js/secret.js
+++ js/secret.js
@@ -4,7 +4,7 @@ var ss = ["",""]
 const ssf = [
     x=>{
         ss[0] += x.toLowerCase()
-        if (!"secretinvasion".includes(ss[0]) && !"shark".includes(ss[0])) {
+        if (!"secretinvasion".includes(ss[0]) && !"shark".includes(ss[0]) && !"bandori".includes(ss[0])) {
             ss[0] = ""
         } else if (ss[0] == "secretinvasion") {
             ss[0] = ""
@@ -22,6 +22,32 @@ const ssf = [
         } else if (ss[0] == "shark") {
             ss[0] = ""
             addNotify(`Shark!!1!<br><img style="width: 100%; height: 100%;" src="https://i.guim.co.uk/img/media/67451b4b5c64652f11eca069b85013f8b31a4244/1549_893_5174_3105/master/5174.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=df139353776471381634b995733c9ebc">`)
+        } else if (ss[0] == "bando" || ss[0] == "bandor" || ss[0] == "bandori") {
+            ss[0] = ""
+            addPopup({
+                html: `
+                    I guess you are inputing "Bandori" in the periodic table.
+					<br>
+					I'm a BanG Dream! Girls Band Party! player.
+					<br>
+					My favorite BanG Dream! band is Poppin'Party.
+					<br>
+					And my favorite BanG Dream! character is Kasumi Toyama.
+					<br>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;---loader3229
+					<br>
+					<a href="https://discord.gg/bangdream" target="_blank">BanG Dream! Official Discord</a>
+					<br>
+					<a href="https://qq1010903229.github.io/bandorichartsEN" target="_blank">My BanG Dream! Fanmade Charts</a>
+					<br>
+					<a href="https://bestdori.com/" target="_blank">Bestdori!</a>
+                `,
+                width: 500,
+                height: 200,
+                otherStyle: {
+                    'font-size': "14px",
+                },
+            })
         }
     },
     _=>{
@@ -31,7 +57,7 @@ const ssf = [
     x=>{
         if (x == ss[1]) {
             localStorage.setItem("imr_secret",ss[1])
-            window.open("https://mrredshark77.github.io/incremental-mass-rewritten/hidden.html","_self")
+            window.open("https://qq1010903229.github.io/incremental-mass-rewritten/hidden.html","_self")
             return true
         }
         return false
